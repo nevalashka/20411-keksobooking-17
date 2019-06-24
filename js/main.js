1a324e02caa7d13196cad3add17369a269d8dd88
nevalashka <b.rukavishnikov@gmail.com>
2019-06-24 10:43:15 +0300

    переменные для объектов 3.1

--- js/main.js
+++ js/main.js
@@ -14,16 +14,15 @@ function getRandomElement(array) {
   return array[getRandom(array.length - 1)];
 }
 
-
-var randomNumberAvatar = function () {
-  return Math.floor(Math.random() * 8);
-};
-
-
 var avatars = [];
 
 for (var i = 1; i < 9; i++) {
   var pic = 'img/avatars/user0' + i + '.png'
   avatars.push(pic);
 }
+
+var x = Math.floor(Math.random() * 1201);
+
+var y = Math.floor(Math.random() * 501) + 130;
+
 
