d16e5daef7c5aee5c5d98642f5dd04787411fc66
nevalashka <b.rukavishnikov@gmail.com>
2019-06-28 18:33:36 +0300

    неработающее задание 3.1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 'use strict';
 
 
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+var showMap = document.querySelector('.map');
+showMap.classList.remove('map--faded');
+
+var similarElement = showMap.querySelector('.map');
+
+var pinTemplate = document.querySelector('#pin')
+  .content
+  .querySelector('.map__pin');
+
+
+
+var avatars = [];
 
 var types = ['palace', 'flat', 'house', 'bungalo'];
+
+var x = Math.floor(Math.random() * 1201);
+
+var y = Math.floor(Math.random() * 501) + 130;
 
 function getRandom(max) {
   return Math.floor(Math.random() * max);
@@ -14,15 +28,75 @@ function getRandomElement(array) {
   return array[getRandom(array.length - 1)];
 }
 
-var avatars = [];
-
 for (var i = 1; i < 9; i++) {
-  var pic = 'img/avatars/user0' + i + '.png';
+  var pic = 'img/avatars/user0' + i + '.png'
   avatars.push(pic);
 }
 
-var x = Math.floor(Math.random() * 1201);
+var pins = [
+  {
+    type: getRandomElement(types),
+    avatar: getRandomElement(avatars),
+    x: Math.floor(Math.random() * 1201),
+    y: Math.floor(Math.random() * 501) + 130
+  },
+  {
+    type: getRandomElement(types),
+    avatar: getRandomElement(avatars),
+    x: Math.floor(Math.random() * 1201),
+    y: Math.floor(Math.random() * 501) + 130
+  },
+  {
+    type: getRandomElement(types),
+    avatar: getRandomElement(avatars),
+    x: Math.floor(Math.random() * 1201),
+    y: Math.floor(Math.random() * 501) + 130
+  },
+  {
+    type: getRandomElement(types),
+    avatar: getRandomElement(avatars),
+    x: Math.floor(Math.random() * 1201),
+    y: Math.floor(Math.random() * 501) + 130
+  },
+  {
+    type: getRandomElement(types),
+    avatar: getRandomElement(avatars),
+    x: Math.floor(Math.random() * 1201),
+    y: Math.floor(Math.random() * 501) + 130
+  },
+  {
+    type: getRandomElement(types),
+    avatar: getRandomElement(avatars),
+    x: Math.floor(Math.random() * 1201),
+    y: Math.floor(Math.random() * 501) + 130
+  },
+  {
+    type: getRandomElement(types),
+    avatar: getRandomElement(avatars),
+    x: Math.floor(Math.random() * 1201),
+    y: Math.floor(Math.random() * 501) + 130
+  },
+];
 
-var y = Math.floor(Math.random() * 501) + 130;
+var renderPin = function (pin) {
+  var pinElement = pinTemplate.cloneNode(true);
+
+  pinElement.querySelector('img').src = pin.avatar;
+  pinElement.querySelector('map__pin').style.position = pin.x;
+  pinElement.querySelector('map__pin').style.position = pin.y;
+  pinElement.querySelector('map__pin').alt = pin.type;
+  return pinElement;
+}
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < pins.length; i++) {
+  fragment.appendChild(renderPin(pins[i]));
+}
+similarElement.appendChild(fragment);
 
 
+/*
+<template id="pin">
+    <button type="button" class="map__pin" style="left: 200px; top: 400px;"><img src="img/avatars/user07.png" width="40" height="40" draggable="false" alt="Метка объявления"></button>
+  </template>
+*/
