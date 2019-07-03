e8ed2c5d729576b781c2dedd3e734c7429464988
nevalashka <b.rukavishnikov@gmail.com>
2019-07-04 00:10:56 +0300

    выполнение задания 3.1

--- js/main.js
+++ js/main.js
@@ -14,13 +14,7 @@ var MAP_HEIGTH_MAX = 501;
 
 var MAP_HEIGTH_MIN = 130;
 
-var avatars = [];
-
 var types = ['palace', 'flat', 'house', 'bungalo'];
-
-var x = Math.floor(Math.random() * MAP_WIDTH);
-
-var y = Math.floor(Math.random() * MAP_HEIGTH_MAX) + MAP_HEIGTH_MIN;
 
 function getRandom(max) {
   return Math.floor(Math.random() * max);
@@ -30,10 +24,15 @@ function getRandomElement(array) {
   return array[getRandom(array.length - 1)];
 }
 
+var avatars = [];
+
 for (var i = 1; i < 9; i++) {
   var pic = 'img/avatars/user0' + i + '.png'
   avatars.push(pic);
 }
+
+
+var pins = [];
 
 var pins = [
   {
@@ -52,10 +51,16 @@ var pins = [
 
 for (var i = 0; i < 9; i++) {
     pins[i] = {
-      avatar: getRandomElement(avatars),
-      type: getRandomElement(types),
-      x: Math.floor(Math.random() * MAP_WIDTH),
-      y: Math.floor(Math.random() * MAP_HEIGTH_MAX) + MAP_HEIGTH_MIN
+      author: {
+        avatar: getRandomElement(avatars)
+      },
+      offer: {
+        type: getRandomElement(types)
+      },
+      location: {
+        x: Math.floor(Math.random() * MAP_WIDTH),
+        y: Math.floor(Math.random() * MAP_HEIGTH_MAX) + MAP_HEIGTH_MIN
+      }
     };
   }
 
@@ -63,10 +68,10 @@ for (var i = 0; i < 9; i++) {
 
 var renderPin = function (pin) {
   var pinElement = pinTemplate.cloneNode(true);
-  pinElement.querySelector('img').setAttribute('src', pin.avatar);
-  pinElement.style.top = pin.x;
-  pinElement.style.left = pin.y;
-  pinElement.querySelector('img').setAttribute('alt', pin.type);
+  pinElement.querySelector('img').setAttribute('src', pin.author.avatar);
+  pinElement.style.left = pin.location.x + 'px';
+  pinElement.style.top = pin.location.y + 'px';
+  pinElement.querySelector('img').setAttribute('alt', pin.offer.type);
   return pinElement;
 }
 
